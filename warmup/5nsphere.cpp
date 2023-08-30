ba1097c695334b2ece50a3819ad7bb64a2f9cced
Lucas Rayder <wwg2tp@virginia.edu>
2023-08-30 15:26:46 -0400

    some comment

---
+++ warmup/5nsphere.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+#include <iostream>
+
+double volume ( double r, int n ) {
+  double V, combo;
+	combo = ( (n/2.0) + 1 );
+	V = ( ( pow( M_PI, (n/2.0) ) /  tgamma( combo )  ) * pow(r, n) );
+	return ( V );
+}
+
+int main () {
+	
+	double rad;
+	int dim;
+
+	rad = 1.2;
+
+	for( dim=0; dim<51; dim++ ) {	
+		printf("%d, %lf, %lf\n", dim, rad, log(volume( rad, dim )) ) ;
+	}
+
+}
